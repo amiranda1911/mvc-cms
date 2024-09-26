29d6d4deb96bf479d142045b7f5d35075a0d24cd
Anderson Miranda <amiranda@amiranda.dev.br>
2024-09-25 21:04:26 -0300

    Adicionado suporte ao Doctrine ORM

--- index.php
+++ index.php
@@ -11,9 +11,13 @@ define('ROOT', dirname(__FILE__));
 define('DS', DIRECTORY_SEPARATOR);
 define('VIEWS_DIR', ROOT.DS.'src'.DS.'app'.DS.'view');
 
+define('APP_SEED', getenv('APP_SEED'));
+
+define('MYSQL_MYSQL_HOST', getenv('MYSQL_MYSQL_HOST'));
+define('MYSQL_USER', getenv('MYSQL_USER'));
+define('MYSQL_PASSWORD', getenv('MYSQL_PASSWORD'));
 define('APP_DATABASE', getenv('APP_DATABASE')); 
 
-define('APP_SEED', getenv('APP_SEED'));
 
 require ROOT.DS.'vendor'.DS.'autoload.php';
 
--- src/core/Persistence.php
+++ src/core/Persistence.php
@@ -2,33 +2,22 @@
 
 namespace core;
 
-class Persistence {
-    
-    private $connection;
+class Persistence { 
+    private $entityManager;
 
     public function __construct() {
-
-        $this->connection = mysqli_connect(
-            getenv('MYSQL_HOST'),
-            getenv('MYSQL_USER'),
-            getenv('MYSQL_PASSWORD'),
-            APP_DATABASE
-        );
-
-        if($this-> connection->connect_error) {
-            die('falha na conexão'. $this->connection->connect_error);
-        }
-        
-        $sql = 'CREATE TABLE IF NOT EXISTIS user (
-                id INT(6) AUTO_INCREMENT PRIMARY KEY,
-                name VARCHAR(255) NOT NULL,
-                weight INT(6) NULL
-                );';
-
+        $params = [
+            'url' => 'mysql:'.MYSQL_USER.':'.MYSQL_PASSWORD.'@'.MYSQL_HOST.'/'.APP_DATABASE
+        ];
+        $paths = [ROOT."/src/app/model"];
+        $isDevMode = true;
+        $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode, null, null, false);
+        $this->entityManager = EntityManager::create($params, $config);
     }
 
 
-    public function __destruct() {
-        $this->connection->close();
+    public function save($object) {
+        $this->entityManager->persist($object);
+        $this->entityManager->flush();
     }
 }
