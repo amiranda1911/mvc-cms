ec1f567346aa39c24861f1d94a99f8b0e3a8334c
Anderson Miranda <amiranda@amiranda.dev.br>
2024-09-27 21:36:22 -0300

    Adicionado suporte ao Plates template engine

--- src/core/View.php
+++ src/core/View.php
@@ -3,25 +3,18 @@
 namespace core;
 
 class View {
+
+    private $template;
     private $view = null;
     private $data = null;
 
     public function __construct($view, $data = array()) {
+        $this->template = new \League\Plates\Engine(VIEWS_DIR);
         $this->view = $view;
         $this->data = $data;
-        $view_path = VIEWS_DIR.DS.$this->view.'.php';
-        //verifica se existe a view e exibe se encontrada 
-        if(file_exists($view_path))
-            $this->view = $view;
-        
-        else {
-            http_response_code(500);
-            die("500 view {$this->view}");
-        } 
     }
 
     public function render() {
-        $data = $this->data;
-        require_once(VIEWS_DIR.DS.$this->view.'.php');
+        echo $this->template->render($this->view, array_merge($_SESSION,$this->data));
     }
 }
