1b90cb263d2f38eed72ba9bd2619ddb6dfb5922e
Anderson Miranda <amiranda@amiranda.dev.br>
2024-10-22 20:46:26 -0300

    feat: change configuration variables names

--- config/config.php
+++ config/config.php
@@ -2,9 +2,7 @@
 if(defined("ROOT"))
     define('VIEWS_DIR', ROOT.DS.'src'.DS.'app'.DS.'view');
 
-define('APP_SEED', getenv('APP_SEED'));
-
 define('MYSQL_HOST', getenv('MYSQL_HOST'));
 define('MYSQL_USER', getenv('MYSQL_USER'));
 define('MYSQL_PASSWORD', getenv('MYSQL_PASSWORD'));
-define('GOLD_APP_DATABASE', getenv('GOLD_APP_DATABASE')); 
+define('APP_DATABASE', getenv('APP_DATABASE')); 
