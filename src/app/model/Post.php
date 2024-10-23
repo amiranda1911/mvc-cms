ac33c98601437ee6321875575dfeecdb6db21b1d
Anderson Miranda <amiranda@amiranda.dev.br>
2024-10-22 21:37:26 -0300

    feat: create Post model

---
+++ src/app/model/Post.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\model;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'posts')]
+
+class Post {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer', name: 'id')]
+    private int|null $id = null;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'posts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user;
+
+    #[ORM\Column(type: 'string', name: 'title')]
+    private $title;
+
+    #[ORM\Column(type: 'string', name: 'content')]
+    private $content;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getContent() {
+        return $this->content;
+    }
+
+    public function setTitle(string $title) {
+        $this->title = $title;
+    }
+
+    public function setContent(string $content) {
+        $this->content = $content;
+    }
+
+}
