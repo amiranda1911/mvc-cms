2527cd9d03ebf4a874b099b53374e851927ff277
Anderson Miranda <amiranda@amiranda.dev.br>
2024-10-28 21:47:06 -0300

    feat: user login blocking on new post controller

--- src/app/controller/PostController.php
+++ src/app/controller/PostController.php
@@ -8,13 +8,22 @@ use core\View;
 class PostController extends Controller{
 
     public function index(){
-        if($_SERVER['REQUEST_METHOD'] == 'GET'){
-            $view = new View('post/new');
-            $view->render();
-        }elseif($_SERVER['REQUEST_METHOD'] == 'POST'){
-            
+        if(isset($this->session->user)){
+            if($_SERVER['REQUEST_METHOD'] == 'GET'){
+                $view = new View('post/new');
+                $view->render();
+            }elseif($_SERVER['REQUEST_METHOD'] == 'POST'){
+                $user = $this->persistence->find("User", $this->session->user);
+                if($user){
+                    $post = new Post(null. $_REQUEST['title'], $_REQUEST['content'], $user);
+                    $this->persistence->save($user);
+                }
+                
+            }
+        }else{
+            header("Location: /login"); 
+            exit; 
         }
-         
     }
 }
 
