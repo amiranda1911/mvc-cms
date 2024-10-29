eaa48a745c854eccec1a0e044487bcb9c26e841f
Anderson Miranda <amiranda@amiranda.dev.br>
2024-10-28 21:49:49 -0300

    fix: change user controller view data showing

--- src/app/view/user/view.php
+++ src/app/view/user/view.php
@@ -1,6 +1,6 @@
 <?php $this->layout('template', ['title' => 'User Profile']) ?>
 <div>
     
-    <h1>Nome:<?= $this->e($name)?></h1>
-    <h2>Quantidade:<?= $this->e($weigth)?></h2>
+    <h1><?= $this->e($name)?></h1>
+    <h2><?= $this->e($email)?></h2>
 </div>
