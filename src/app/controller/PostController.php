1b13a869b24342ab53e9a174f5783aabd038bdb3
Anderson Miranda <amiranda@amiranda.dev.br>
2024-10-29 20:31:12 -0300

    fix: post content column type

--- src/app/controller/PostController.php
+++ src/app/controller/PostController.php
@@ -4,19 +4,21 @@ namespace app\controller;
 
 use core\Controller;
 use core\View;
+use app\model\Post;
 
 class PostController extends Controller{
 
     public function index(){
-        if(isset($this->session->user)){
+        if($this->session->user){
             if($_SERVER['REQUEST_METHOD'] == 'GET'){
                 $view = new View('post/new');
                 $view->render();
             }elseif($_SERVER['REQUEST_METHOD'] == 'POST'){
-                $user = $this->persistence->find("User", $this->session->user);
+                $user = $this->persistence->find("app\model\User", $this->session->user);
                 if($user){
                     $post = new Post(null. $_REQUEST['title'], $_REQUEST['content'], $user);
-                    $this->persistence->save($user);
+                    $this->persistence->save($post);
+                    header("Location: /");        
                 }
                 
             }
