a2ac987d05570a22e72a6ae62ff67b96ba6f66c9
Anderson Miranda <amiranda@amiranda.dev.br>
2024-10-29 20:31:45 -0300

    feat: add home controller index post data find

--- src/app/controller/HomeController.php
+++ src/app/controller/HomeController.php
@@ -8,7 +8,16 @@ use core\View;
 class HomeController extends Controller{
 
     public function index(){
-        $view = new View('home/index');
+        $postsObject = $this->persistence->findAll('app\model\Post');
+        $posts = [];
+        foreach ($postsObject as $post){
+            $newUser = array();
+            $newUser['id'] = $post->getId();
+            $newUser['title'] = $post->getTitle();
+            $newUser['content'] = $post->getContent();
+            array_push($users, $newUser);
+        }
+        $view = new View('home/index', ['posts' => $posts]);
         $view->render(); 
     }
 }
