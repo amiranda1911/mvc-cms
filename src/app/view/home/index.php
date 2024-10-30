82b6b9426fbd22bb67fdd083481c5a6d38f1d3c6
Anderson Miranda <amiranda@amiranda.dev.br>
2024-10-29 21:37:53 -0300

    feat: home showing all posts

--- src/app/view/home/index.php
+++ src/app/view/home/index.php
@@ -1,1 +1,15 @@
-<?php $this->layout('template', ['title' => 'User Profile']) ?>
+<?php $this->layout('template', ['title' => 'CMS']) ?>
+
+<section id="posts">
+<?php foreach ($posts as $post):?>
+    <article class="post">
+        <h2><a href="/post/view/<?= $post['id'] ?>"><?= $post['title'] ?></a></h2>
+        <p><?= $post['content'] ?></p>
+
+    </article>
+
+<?php endforeach; ?>
+
+</section>
+
+
