ea4002825fc5adbc0310c4a13359331d5bbbff92
Anderson Miranda <amiranda@amiranda.dev.br>
2024-10-29 21:18:21 -0300

    feat: add view action to post controller

--- src/app/controller/PostController.php
+++ src/app/controller/PostController.php
@@ -27,5 +27,27 @@ class PostController extends Controller{
             exit; 
         }
     }
+
+    public function view(...$params) {
+        if(isset($params[0]) && (intval($params[0]) > 0)){
+            $postObject = $this->persistence->find('app\model\Post', $params[0]);
+            $userObject = $this->persistence->find('app\model\User', $postObject->getUser()->getId());
+            
+            $post = [
+                'title' => $postObject->getTitle(),
+                'content' => $postObject->getContent()
+            ];
+
+            $user = [
+                'id' => $userObject->getId(),
+                'name' => $userObject->getName()
+            ];
+            
+            $view = new View('post/view',
+                ['post' => $post, 'user' => $user]
+            );
+            $view->render();
+        }
+    }
 }
 
